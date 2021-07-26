bec1803b9abfa00d051350d7f174a844e7c8dd38
ronniew <rwaebers@gmail.com>
2021-07-26 14:32:36 +0200

    🎨Format document with prettier

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,11 +1,11 @@
-const { Request, Response, NextFunction } = require('express')
-const { Post } = require('../models');
+const { Request, Response, NextFunction } = require("express");
+const { Post } = require("../models");
 
 /**
  * Create a new post
- * @param {Request} req 
- * @param {Response} res 
- * @param {NextFunction} next 
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
 exports.create = async (req, res, next) => {
   const { title, content } = req.body;
@@ -13,36 +13,36 @@ exports.create = async (req, res, next) => {
   if (!title) {
     res.status(400);
     return res.json({
-      message: "Title is required."
-    })
+      message: "Title is required.",
+    });
   }
 
   if (!content) {
     res.status(400);
     return res.json({
-      message: "Content is required."
-    })
+      message: "Content is required.",
+    });
   }
 
   try {
     const post = await Post.create({
       title,
-      content
+      content,
     });
 
     res.status(201);
     return res.json(post);
   } catch (error) {
     // Forward an error message to the errorHandler middleware.
-    next(new Error("An error occurred whilst trying to create the post."))
+    next(new Error("An error occurred whilst trying to create the post."));
   }
-}
+};
 
 /**
  * Find all posts
- * @param {Request} req 
- * @param {Response} res 
- * @param {NextFunction} next 
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
 exports.findAll = async (req, res, next) => {
   try {
@@ -51,15 +51,15 @@ exports.findAll = async (req, res, next) => {
     return res.json(posts);
   } catch (error) {
     // Forward an error message to the errorHandler middleware.
-    next(new Error(`An error occurred whilst trying to find all posts.`))
+    next(new Error(`An error occurred whilst trying to find all posts.`));
   }
-}
+};
 
 /**
  * Find a single post
- * @param {Request} req 
- * @param {Response} res 
- * @param {NextFunction} next 
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
 exports.findOne = async (req, res, next) => {
   const { id } = req.params;
@@ -75,15 +75,19 @@ exports.findOne = async (req, res, next) => {
     return res.json(post);
   } catch (error) {
     // Forward an error message to the errorHandler middleware.
-    next(new Error(`An error occurred whilst trying to find a post with ID: ${id}.`))
+    next(
+      new Error(
+        `An error occurred whilst trying to find a post with ID: ${id}.`
+      )
+    );
   }
-}
+};
 
 /**
  * Update a post
- * @param {Request} req 
- * @param {Response} res 
- * @param {NextFunction} next 
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
 exports.update = async (req, res, next) => {
   const { id } = req.params;
@@ -93,7 +97,7 @@ exports.update = async (req, res, next) => {
 
   if (!title && !content) {
     res.status(411);
-    return next(new Error("Data to update the post cannot be empty."))
+    return next(new Error("Data to update the post cannot be empty."));
   }
 
   try {
@@ -107,15 +111,19 @@ exports.update = async (req, res, next) => {
     return res.json(post);
   } catch (error) {
     // Forward an error message to the errorHandler middleware.
-    next(new Error(`An error occurred whilst trying to update a post with ID: ${id}.`))
+    next(
+      new Error(
+        `An error occurred whilst trying to update a post with ID: ${id}.`
+      )
+    );
   }
-}
+};
 
 /**
  * Delete a post
- * @param {Request} req 
- * @param {Response} res 
- * @param {NextFunction} next 
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
  */
 exports.delete = async (req, res, next) => {
   const { id } = req.params;
@@ -131,6 +139,10 @@ exports.delete = async (req, res, next) => {
     return res.json(post);
   } catch (error) {
     // Forward an error message to the errorHandler middleware.
-    next(new Error(`An error occurred whilst trying to delete a post with ID: ${id}.`))
+    next(
+      new Error(
+        `An error occurred whilst trying to delete a post with ID: ${id}.`
+      )
+    );
   }
-}
+};
